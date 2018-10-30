d4b6388634e7a3095a2aff0b70f5cca5469809e4
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-10-30 19:25:48 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw doget '/login' => 'sessions#new'
 
   root 'welcome#home'
 
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
